af0cc55ffc4de33b5b565ac3ce35a832683a3943
Toni Itkonen <toni.itkonen@icloud.com>
2022-05-30 11:10:45 +0300

    + Add: Dashboard view with coloured bar chart

--- MyDay/MyDay/Views/DashboardView.swift
+++ MyDay/MyDay/Views/DashboardView.swift
@@ -1,16 +1,61 @@
-//
-//  DashboardView.swift
-//  MyDay
-//
-//  Created by Toni Itkonen on 30.5.2022.
-//
-
 import SwiftUI
 
 struct DashboardView: View {
+  @Environment(\.managedObjectContext) var managedObjContext
+  @State private var title = ""
+  @State var mood: Double = 0
+  @State var color: Color = .gray
+  
+  @FetchRequest(sortDescriptors: [SortDescriptor(\.mood)]) var day: FetchedResults<MyDayEntity>
+  
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      NavigationView {
+        List {
+            ForEach(day) { day in
+              VStack(alignment: .leading) {
+                HStack(alignment: .top) {
+                  Text(day.title ?? "Unknown name")
+                    .onAppear {
+                      mood = day.mood
+                      title = day.title!
+                    }
+                  Spacer()
+                  Text(calcTimeSince(date:day.date!))
+                    .foregroundColor(.gray)
+                    .font(.caption)
+                    .italic()
+                }///_HStack
+                ZStack {
+                  Rectangle()
+                    .foregroundColor(colouredBars(mood:day.mood))
+                    .frame(width: day.mood * 3, height: 15, alignment: .trailing)
+                    .cornerRadius(5)
+                  Text("\(Int(day.mood))")
+                    .foregroundColor(.white)
+                    .font(.caption)
+                }///-ZStack
+              }///-VStack
+            }///ForEach Ends
+        }///-List
+        .navigationTitle("Charts")
+      }
+    } ///_Body
+  
+  func colouredBars(mood: Double) -> Color {
+    if mood >= 60 {
+      print("color green")
+      return .green
+    } else if mood >= 30 {
+      print("color yellow")
+      return .yellow
+    } else {
+      print("color red")
+      return .red
     }
+     return .clear
+  }
+  
+  
 }
 
 struct DashboardView_Previews: PreviewProvider {
